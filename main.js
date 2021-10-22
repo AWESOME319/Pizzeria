49f36f837defe6f329d2846982d353b09411240a
AWESOME319 <83673925+AWESOME319@users.noreply.github.com>
2021-10-22 19:31:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ menu_list_array = ["Veg Margherita Pizza","New York style Pizza","Cheese Pizza",
 "Garlic Fingers","Tomato Pie","California-Style Pizza","Italian style pizza"];
 
 function getmenu(){
-var htmldata;
+var htmldata=[];
 menu_list_array.sort();
         for(var i=0;i<menu_list_array.length;i++){
             htmldata=htmldata+ menu_list_array[i] + '<br>'
         }
-        document.getElementById(" display_menu ").innerHTML = htmldata;
+        document.getElementById("display_menu").innerHTML = htmldata;
 
 }
 
@@ -17,7 +17,7 @@ var item=document.getElementById("add_item").value;
          menu_list_array.sort();
         htmldata=""
         for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+            htmldata=htmldata+ menu_list_array[i]+'<br>';
         }
          document.getElementById("display_addedmenu").innerHTML = htmldata;		
       }
